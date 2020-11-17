1cb3a4ca4da51f02cc6bb023a90acf8ba1c6ca2b
Aleksey Koshkin <ekbdizzy@yandex.ru>
2020-11-17 16:35:39 +0300

    added jinja2

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from datetime import datetime
 
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+year = datetime.now().year - 1920
+
+rendered_page = template.render(
+    year=year,
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
