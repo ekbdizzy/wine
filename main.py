b3726fcd37af908d86ad4f2dd9d6ec90f9efc9f0
Aleksey Koshkin <ekbdizzy@yandex.ru>
2020-12-22 18:52:57 +0300

    finished step 11

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from datetime import datetime
 
+from read_from_xls import read_vines_from_xslx
+
 env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml'])
 )
+
+vines = [vine for vine in read_vines_from_xslx('xls/wine2.xlsx') if vine['Сорт']]
+print(vines)
 
 template = env.get_template('template.html')
 
@@ -13,6 +18,7 @@ year = datetime.now().year - 1920
 
 rendered_page = template.render(
     year=year,
+    vines=vines,
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
